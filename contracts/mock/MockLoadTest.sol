47deba13ad56a73d43de849977fc427b0cdbb5a4
omnus <omnusunmo@gmail.com>
2025-07-28 08:43:21 +1200

    Update MockLoadTest.sol

--- contracts/mock/MockLoadTest.sol
+++ contracts/mock/MockLoadTest.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "../consumer/ArrngConsumer.sol";
 
-contract MockLoadTest is ArrngConsumer, Ownable {
+contract MockLoadTest is ArrngConsumer {
   mapping(uint256 => uint256) public requestIdToResult;
 
   constructor(address arrngController_) ArrngConsumer(arrngController_) {}
 
-  function loadTest(uint256 numberOfRequests_) public payable onlyOwner {
+  function loadTest(uint256 numberOfRequests_) public payable {
     uint256 valuePerCall = msg.value / numberOfRequests_;
     for (uint256 i = 0; i < numberOfRequests_; i++) {
       arrngController.requestRandomWords{value: valuePerCall}(1);

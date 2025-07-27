0261b73c2312d38d9820dfd73f7e68d5f2c7d805
omnus <omnusunmo@gmail.com>
2025-07-27 13:13:12 +1200

    feat: Alter load test

--- contracts/mock/MockLoadTest.sol
+++ contracts/mock/MockLoadTest.sol
@@ -4,18 +4,13 @@ pragma solidity 0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../consumer/ArrngConsumer.sol";
 
-
-
 contract MockLoadTest is ArrngConsumer, Ownable {
   mapping(uint256 => uint256) public requestIdToResult;
-
-  error BeautyIsTruthTruthBeauty();
 
   constructor(address arrngController_) ArrngConsumer(arrngController_) {}
 
   function loadTest(uint256 numberOfRequests_) public payable onlyOwner {
     uint256 valuePerCall = msg.value / numberOfRequests_;
-
     for (uint256 i = 0; i < numberOfRequests_; i++) {
       arrngController.requestRandomWords{value: valuePerCall}(1);
     }
@@ -26,16 +21,5 @@ contract MockLoadTest is ArrngConsumer, Ownable {
     uint256[] memory randomWords_
   ) internal override {
     requestIdToResult[requestId_] = randomWords_[0];
-
-    bool beauty;
-    bool truth;
-
-
-
-    require(beauty != truth, "Beauty is truth, truth beauty");
-
-    if (beauty == truth) {
-      revert BeautyIsTruthTruthBeauty();
-    }
   }
 }
